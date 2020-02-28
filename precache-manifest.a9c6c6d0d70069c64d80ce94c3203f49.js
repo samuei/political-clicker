0fa34a292a9df6aca4ad8a8fa41a1d086c30fa5f
Lamb <samuei@home.ku.edu>
2020-02-28 08:06:57 -0600

    Updates

---
+++ precache-manifest.a9c6c6d0d70069c64d80ce94c3203f49.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "857462ea69767d5e9cdbb3630f07c7fc",
+    "url": "/political-clicker/index.html"
+  },
+  {
+    "revision": "e9993096501c3eeba607",
+    "url": "/political-clicker/static/css/main.66243517.chunk.css"
+  },
+  {
+    "revision": "61a13f4b4dd3e06d8d77",
+    "url": "/political-clicker/static/js/2.afd35491.chunk.js"
+  },
+  {
+    "revision": "e9993096501c3eeba607",
+    "url": "/political-clicker/static/js/main.6f5580cc.chunk.js"
+  },
+  {
+    "revision": "b6e43fff46871fdd6e01",
+    "url": "/political-clicker/static/js/runtime-main.85fc64f4.js"
+  }
+]);
