dfb88fd5dcc48dd97ade0ab15196dd1307f297b0
samuei <samuei@users.noreply.github.com>
2020-11-06 14:42:41 -0600

    Add a canvassing action
    Why not? Do a good deed, now and then.

--- src/util/action_constants.js
+++ src/util/action_constants.js
@@ -4,5 +4,6 @@ export const CANVASING_ACTION = [
 	'After a dozen doors slammed in your face, somebody finally throws money at you first.',
 	'You find some cash stuffed between your couch cushions.',
 	'During a break in canvassing, an old lady mistakes you for a beggar and gives you a little money. Score!',
-	'Somebody gives you cash to skip ahead of you at the line for the printer at the library.'
+	'Somebody gives you cash to skip ahead of you at the line for the printer at the library.',
+	'You return a lost wallet and the owner gives you some of his cash!'
 ];
